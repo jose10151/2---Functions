520d5f630c9da83b55436898a87a449cbef3b10c
Jose Trujillo <joseh.trujillo11@gmail.com>
2023-09-28 13:51:55 -0400

    DONEEEEE

--- Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -8,7 +8,36 @@
 //:  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
 import Foundation
 
+func magicEightBall(){
+    let randomNum = Int.random(in: 0...4)
+    
+    switch randomNum {
+    case 0:
+        print("the num is 0")
+        
+    case 1:
+        print("the num is 1")
 
+    case 2:
+        print("the num is 2")
+
+    case 3:
+        print("the num is 3")
+
+    case 4:
+        print("the num is 4")
+
+        
+    default:
+        print("erroe")
+
+    }
+}
+
+magicEightBall()
+magicEightBall()
+magicEightBall()
+magicEightBall()
 /*:
 page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
  */
--- Lab - Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
+++ Lab - Functions.playground/Pages/5. Exercise - Return Values.xcplaygroundpage/Contents.swift
@@ -3,10 +3,18 @@
 
  Write a function called `greeting` that takes a `String` argument called name, and returns a `String` that greets the name that was passed into the function. I.e. if you pass in "Dan" the return value might be "Hi, Dan! How are you?" Use the function and print the result.
  */
+func greeting(_ name:String) ->String {
+     "Hi, \(name)! How are you?"
+}
 
+greeting("Jose")
 
 //:  Write a function that takes two `Int` arguments, and returns an `Int`. The function should multiply the two arguments, add 2, then return the result. Use the function and print the result.
+func multiply(_ num1:Int, _ num2:Int) {
+    num1 * num2 + 2
+}
 
+multiply(2,3)
 
 /*:
 [Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Separating Functions](@next)
